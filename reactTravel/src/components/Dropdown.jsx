56eed481dfe00daa1e33c76b810d22401044d314
PixelHalide <taintedxenon@gmail.com>
2025-06-16 15:02:53 +0400

    Dropdown is now link

--- reactTravel/src/components/Dropdown.jsx
+++ reactTravel/src/components/Dropdown.jsx
@@ -1,10 +1,17 @@
+import { Link } from 'react-router-dom';
 
 const Dropdown = ({contents}) => {
   return (
     <div className='flex flex-col'>
-        <ul className='flex flex-col text-center'>
-            {contents.map((content, idx) => (<li key={idx}>{content}</li>))}
-        </ul>
+      <ul className='flex flex-col text-center'>
+        {contents.map((content, idx) => (
+          <li key={idx} className="hover:bg-gray-100 py-2">
+            <Link to={`/${content.toLowerCase()}`} className="block w-full">
+              {content}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
