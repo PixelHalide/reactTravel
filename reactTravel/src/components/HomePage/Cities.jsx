c6708815da2e4e3088614f04acc11d1e0901d05a
PixelHalide <taintedxenon@gmail.com>
2025-06-17 19:01:58 +0400

    Seperation of components

--- reactTravel/src/components/HomePage/Cities.jsx
+++ reactTravel/src/components/HomePage/Cities.jsx
@@ -1,10 +1,56 @@
 
+import HotelCard from "./HotelCard.jsx"
+import atlantis from '../../assets/atlantisDubai.webp'
+import nora from '../../assets/nora.jpg'
+import royal from '../../assets/royal.jpg'
+import kokotel from '../../assets/kokotel.jpg'
 
+const dummyData = [
+  {
+    image: kokotel,
+    name: 'Kokotel Hakodate',
+    rating: 3.8,
+    reviews: 23,
+  },
+  {
+    image: atlantis,
+    name: 'Atlantis Dubai',
+    rating: 4.8,
+    reviews: 1023,
+  },
+  {
+    image: nora,
+    name: 'Nora Buri Resort & Spa',
+    rating: 4.5,
+    reviews: 2145,
+  },
+  {
+    image: royal,
+    name: 'Royal Phuket City Hotel',
+    rating: 3.5,
+    reviews: 340,
+  },
+];
 const Cities = () => {
+    const handleClick = (name) => {
+    alert(`Clicked on ${name}`)
+  }
   return (
-    <div className="text-2xl mt-4 ml-16">
-        Iconic places you need to see
+    <div className="text-2xl mt-6 ml-16">
+        Hotels that'll make you feel at home
+      <div className="flex flex-wrap justify-center gap-6 p-6">
 
+        {dummyData.map((hotel, index) => (
+        <HotelCard
+          key={index}
+          hotelImage={hotel.image}
+          hotelName={hotel.name}
+          hotelRating={hotel.rating}
+          hotelReviews={hotel.reviews}
+          onClick={() => handleClick(hotel.name)}
+        />
+        ))}
+      </div>
     </div>
   )
 }
--- reactTravel/src/components/HomePage/Home.jsx
+++ reactTravel/src/components/HomePage/Home.jsx
@@ -2,30 +2,6 @@ import TopBanner from "./TopBanner.jsx"
 import Cities from "./Cities"
 import Gallery from "./Gallery.jsx"
 import HotelCard from "./HotelCard.jsx"
-import atlantis from '../../assets/atlantisDubai.webp'
-import nora from '../../assets/nora.jpg'
-import royal from '../../assets/royal.jpg'
-
-const dummyData = [
-    {
-    image: atlantis,
-    name: 'Atlantis Dubai',
-    rating: 4.8,
-    reviews: 1023,
-  },
-  {
-    image: nora,
-    name: 'Nora Buri Resort & Spa',
-    rating: 4.5,
-    reviews: 2145,
-  },
-  {
-    image: royal,
-    name: 'Royal Phuket City Hotel',
-    rating: 3.5,
-    reviews: 340,
-  },
-];
 
 function Home () {
 
@@ -38,20 +14,7 @@ function Home () {
     <TopBanner />
     <Cities />
 
-    <div className="flex flex-wrap justify-center gap-6 p-6">
 
-      {dummyData.map((hotel, index) => (
-      <HotelCard
-        key={index}
-        hotelImage={hotel.image}
-        hotelName={hotel.name}
-        hotelRating={hotel.rating}
-        hotelReviews={hotel.reviews}
-        onClick={() => handleClick(hotel.name)}
-      />
-      ))}
-
-    </div>
     </>
   )
 }
