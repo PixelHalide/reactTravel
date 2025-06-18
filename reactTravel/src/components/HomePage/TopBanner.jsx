b9d55e36407fc77f1fa215c2c75788072a1d01ea
PixelHalide <taintedxenon@gmail.com>
2025-06-18 21:08:10 +0400

    Text outline

--- reactTravel/src/components/HomePage/TopBanner.jsx
+++ reactTravel/src/components/HomePage/TopBanner.jsx
@@ -4,7 +4,7 @@ const TopBanner = () => {
   return (
     <div className="justify-center relative transition-all">
         <img src={italyImageUrl} alt="Italy" className="max-w-full blur-xs"/>
-        <div className='absolute inset-0 flex flex-col items-center justify-center text:4xl md:text-6xl text-white'>
+        <div className='absolute inset-0 flex flex-col items-center justify-center text:4xl md:text-6xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
           <p className='block'>Redefining Travel -</p>
           <p className='block'>One step at a Time.</p>
           <div className='text-lg md:text-2xl mt-14'>
