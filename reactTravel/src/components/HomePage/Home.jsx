0829fa590cd3235ad65a291918c070f1debdbac5
PixelHalide <taintedxenon@gmail.com>
2025-06-18 21:43:40 +0400

    Remove more

--- reactTravel/src/components/HomePage/Home.jsx
+++ reactTravel/src/components/HomePage/Home.jsx
@@ -1,13 +1,8 @@
 import TopBanner from "./TopBanner.jsx"
 import CardArray from "./CardArray.jsx"
 import Gallery from "./Gallery.jsx"
-import HotelCard from "./LocationCard.jsx"
 
 function Home () {
-
-  const handleClick = (name) => {
-    alert(`Clicked on ${name}`)
-  }
 
   return (
     <>
