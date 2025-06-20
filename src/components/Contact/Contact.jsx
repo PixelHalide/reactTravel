602cf5e9d896b6a60640c747d83aab5d1690d534
PixelHalide <taintedxenon@gmail.com>
2025-06-20 23:22:37 +0400

    Contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,8 +7,17 @@ function Contact() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div>
+    <div className="min-h-screen flex flex-col items-center justify-center px-6 py-12 text-gray-800 dark:text-neutral-200">
+      <div className="max-w-4xl mx-auto text-center mb-12">
+        <h1 className="text-4xl font-bold mb-6">Contact Us</h1>
+        <p className="text-lg mb-4">
+          Have questions, suggestions, or need help booking tickets? We're here to assist you! ✈️
+        </p>
+        <p className="mb-4">
+          Reach out to us for any queries, feedback, or support regarding your travel plans. Your journey starts here!
+        </p>
+      </div>
+      <div className=" mx-auto">
         <h2 className="text-3xl font-bold mb-6 text-center dark:text-neutral-200">Contact Us</h2>
         <Form onSubmit={handleFormSubmit} />
       </div>
@@ -16,4 +25,4 @@ function Contact() {
   );
 }
 
-export default Contact
+export default Contact;
